d0307d213541d7c63526899b86e5d63610e6921e
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-05-04 09:27:47 +0200

    Fixes the way the input fields validates that the inputs are numbers and not letters.

--- src/Application/MVVM/View/CharacterSheet/CharacterViewController.java
+++ src/Application/MVVM/View/CharacterSheet/CharacterViewController.java
@@ -4,7 +4,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.input.KeyEvent;
+
+import java.text.DecimalFormat;
+import java.text.ParsePosition;
+import java.util.function.UnaryOperator;
 
 public class CharacterViewController
 {
@@ -32,16 +37,32 @@ public class CharacterViewController
     intMod.textProperty().bind(viewModel.intModProperty());
     wisMod.textProperty().bind(viewModel.wisModProperty());
     charMod.textProperty().bind(viewModel.charModProperty());
+
+    DecimalFormat format = new DecimalFormat("#");
+    UnaryOperator<TextFormatter.Change> filter = c -> {
+      if (c.getControlNewText().isEmpty()){
+        return c;
+      }
+      ParsePosition parsePosition = new ParsePosition(0);
+      Object object = format.parse(c.getControlNewText(),parsePosition);
+
+      if ((object == null) || ((parsePosition.getIndex()) < (c.getControlNewText().length()))){
+        return null;
+      } else {
+        return c;
+      }
+    };
+
+    strField.setTextFormatter(new TextFormatter<>(filter));
+    dexField.setTextFormatter(new TextFormatter<>(filter));
+    conField.setTextFormatter(new TextFormatter<>(filter));
+    intField.setTextFormatter(new TextFormatter<>(filter));
+    wisField.setTextFormatter(new TextFormatter<>(filter));
+    charField.setTextFormatter(new TextFormatter<>(filter));
   }
 
   public void onStatEnter(ActionEvent actionEvent)
   {
     viewModel.setStat((TextField) actionEvent.getSource());
-  }
-
-  public void onStatInput(KeyEvent keyEvent)
-  {
-    if (!keyEvent.getCharacter().matches("\\d"))
-      ((TextField)keyEvent.getSource()).setText(((TextField)keyEvent.getSource()).getText().replaceAll("[^\\d]",""));
   }
 }
--- src/Application/MVVM/View/CharacterSheet/CharacterViewModel.java
+++ src/Application/MVVM/View/CharacterSheet/CharacterViewModel.java
@@ -55,19 +55,24 @@ public class CharacterViewModel
 
   public void setStat(TextField source)
   {
-    switch (source.getId()){
-      case "strField" ->
-          strMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
-      case "dexField" ->
-          dexMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
-      case "conField" ->
-          conMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
-      case "intField" ->
-          intMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
-      case "wisField" ->
-          wisMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
-      case "charField" ->
-          charMod.setValue((Integer.parseInt(source.getText())-10)/2 + "");
+    if (!source.getText().isEmpty()){
+      switch (source.getId())
+      {
+        case "strField" -> strMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        case "dexField" -> dexMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        case "conField" -> conMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        case "intField" -> intMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        case "wisField" -> wisMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        case "charField" -> charMod.setValue(
+            (Integer.parseInt(source.getText()) - 10) / 2 + "");
+        default -> {
+        }
+      }
     }
 
     System.out.println(source.getId());
