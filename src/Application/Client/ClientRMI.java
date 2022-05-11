353877baf2ff7ac1aa5ddf2b600e872220b100f7
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2022-05-11 10:06:55 +0200

    Update ClientRMI.java

--- src/Application/Client/ClientRMI.java
+++ src/Application/Client/ClientRMI.java
@@ -11,11 +11,10 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Client implements ClientRMI, ClientLogin
+public class Client implements ClientRMI
 {
   private final IServer server;
-  private UserID userID;
-
+  
   public Client() throws RemoteException, NotBoundException
   {
     UnicastRemoteObject.exportObject(this,0);
@@ -32,23 +31,5 @@ public class Client implements ClientRMI, ClientLogin
   @Override public Character getCharacter(String name) throws RemoteException
   {
     return server.getCharacter(name);
-  }
-
-  @Override public void setUserID(UserID userID)
-  {
-    this.userID = userID;
-    System.out.println(this.userID);
-  }
-
-  @Override public void onExit()
-  {
-    try
-    {
-      UnicastRemoteObject.unexportObject(this,true);
-    }
-    catch (NoSuchObjectException e)
-    {
-      e.printStackTrace();
-    }
   }
 }
