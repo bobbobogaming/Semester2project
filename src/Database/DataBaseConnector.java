91ff4e0087e3ac2e9b5a6c306e9ddddae1b6c43d
morte <morten_oel@hotmail.com>
2022-05-12 11:38:23 +0200

    Lavet nogle ting på forbindelsen imellem server og Database (lavet en lappeløsning for at kunne finde alle chacaters)

--- src/Database/DataBaseConnector.java
+++ src/Database/DataBaseConnector.java
@@ -1,6 +1,8 @@
 package Database;
 
+import Application.Client.UserID;
 import Application.MVVM.Model.character.Character;
+import Application.MVVM.Model.character.Stats;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -82,24 +84,23 @@ public class DataBaseConnector implements IDatabaseConnector {
   public void deleteDataFromDataBase(String table, ArrayList<String> coloumns, ArrayList<Object> equals) {
     try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
       if (coloumns.size() == equals.size()) {
-        String query = "insert into " + table + " values (";
+        String query = "Delete from " + table + " where ";
+
 
         //first we make the number of elements that we need to be able to
         for (int i = 0; i < coloumns.size(); i++) {
           if (i != 0) {
-            query += "and ? == ?";
+            query += " and "+ coloumns.get(i) +" = ? ";
           } else {
-            query += "? == ?";
+            query += coloumns.get(i) + " = ?";
           }
         }
-        query += ")";
+
         PreparedStatement stmt = conn.prepareStatement(query);
         //stmt.setObject(1, table);
         for (int i = 0; i < coloumns.size(); i++) {
-          stmt.setObject(i*2 + 1, coloumns.get(i));
-          stmt.setObject(i*2 + 2, equals.get(i));
+          stmt.setObject(i + 1, equals.get(i));
         }
-
 
         stmt.execute();
 
@@ -110,32 +111,89 @@ public class DataBaseConnector implements IDatabaseConnector {
     }
   }
 
+  @Override
+  public void selectAllDataFromTableDatabase(String table) {
+
+    try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+      String query = "select * from " + table;
+
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
 
 
-
-  public static void main(String[] args) {
-    //    DatabaseWrapper.addCharacter("Anders", "H");
-    //    DatabaseWrapper.addCharacter("Simon", "L");
-    ArrayList<String> player = new ArrayList<>();
-
-    player.add("Morten");
-    //DataBaseConnector.addDataToDataBase("Player",player);
-
-    ArrayList<Object> data = new ArrayList<>();
-    data.add(10);
-    data.add(11);
-    data.add(12);
-    data.add(13);
-    data.add(14);
-    data.add(15);
-    data.add(player.get(0));
-    data.add("BOB");
-    data.add(1);
-    data.add("class");
-    data.add(50);
-    DataBaseConnector dataBaseConnector = new DataBaseConnector();
-    dataBaseConnector.addDataToDataBase("Character",data);
-    //System.out.println(DataBaseConnector.getAllCharacters());
-
+      while (rs.next()) {
+        System.out.println(rs.getArray(1));
+        //System.out.println(rs.);
+        //characters += "First name: " + rs.getString("fname")
+        //        + ", Last name: " + rs.getString("lname") + "\n";
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
+
+  public ArrayList<Character> getAllCharacterFromUser(UserID userID){
+    ArrayList<Character> arrayListCharacter = new ArrayList<>();
+    try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+
+      String query = "select * from character where playerid = " + "'" + userID.getName()+
+              "'" ;
+
+      Statement stmt = conn.createStatement();
+
+      ResultSet rs = stmt.executeQuery(query);
+
+
+      while (rs.next()) {
+        int constitution = rs.getInt("constitution");
+        int wisdom = rs.getInt("wisdom");
+        int charisma = rs.getInt("charisma");
+        int intelligence = rs.getInt("intelligence");
+        int strength = rs.getInt("strength") ;
+        int dexterity = rs.getInt("Dexterity");
+        String characterName = rs.getString("characterName");
+
+        int level = rs.getInt("level");
+        String clas = rs.getString("class");
+        int maxHp = rs.getInt("maxHp");
+
+        Stats stats = new Stats(strength,dexterity,constitution,intelligence,wisdom,charisma);
+        arrayListCharacter.add(new Character(stats,characterName));
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return arrayListCharacter;
+  }
+
+  @Override
+  public ArrayList selectColumnsFromTableDataBase(ArrayList<String> columnsName, String table) {
+    ArrayList<Object> returnvalue = new ArrayList<>();
+
+    try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+
+      String query = "select * from " + table;
+
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery(query);
+
+
+      while (rs.next()) {
+        for (String columns:columnsName
+             ) {
+          returnvalue.add(rs.getObject(columns));
+        }
+        System.out.println(rs.getArray(1));
+        //System.out.println(rs.);
+        //characters += "First name: " + rs.getString("fname")
+        //        + ", Last name: " + rs.getString("lname") + "\n";
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return returnvalue;
+  }
+
+
 }
